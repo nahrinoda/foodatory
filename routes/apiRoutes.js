c9da0eb70036e2f8d78b7e0cfdf33cd2e3cdde33
nahrinoda <32439687+nahrinoda@users.noreply.github.com>
2018-08-19 22:58:35 -0400

    Applied Travis testing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,16 @@
 var db = require("../models");
-var ingredientSearcher = require('../ingredientSearcher.js')
+var ingredientSearcher = require("../ingredientSearcher.js");
 
 module.exports = function(app) {
   // Get all examples
   app.get("/api/recipes/:ingredients", function(req, res) {
-    console.log("anything")
-    var ingredients= req.params.ingredients
-    
-    ingredients = ['egg', 'milk']
-    ingredientSearcher(ingredients)
-      .then(function (recipes) {  
-        res.json(recipes);
-      });
+    console.log("anything");
+    var ingredients = req.params.ingredients;
 
+    ingredients = ["egg", "milk"];
+    ingredientSearcher(ingredients).then(function(recipes) {
+      res.json(recipes);
+    });
   });
 
   // Create a new example
@@ -24,7 +22,9 @@ module.exports = function(app) {
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.json(dbExample);
     });
   });
